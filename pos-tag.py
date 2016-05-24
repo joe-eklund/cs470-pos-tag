99bb403a48f23158bbd507763ee58618b20810d3
Jacob Gillespie <jacobgillespie@gmail.com>
2016-05-24 12:43:27 -0600

    Saved off the dictionary produced by the training_dataset.

--- pos-tag.py
+++ pos-tag.py
@@ -1,5 +1,7 @@
 import random
 import sys
+import json
+import os.path
 
 class BiGram:
 
@@ -7,33 +9,29 @@ class BiGram:
     debug = True
 
     def __init__(self, source):
-        previous = None
-        with open(source, 'r') as f:
-            for line in f:
-                for word in line.split():
-                    current = word.rpartition('_')[0]
-                    if previous is not None:
-                        if previous in self.wordMap.keys() is not None:
-                            if current in self.wordMap[previous].keys() is not None:
-                                self.wordMap[previous][current] += 1
-                            else:
-                                self.wordMap[previous][current] = 1
-                        else:
-                            self.wordMap[previous] = {current : 1}
-                    previous = current
+        datafile = 'bigram_data.txt'
 
-    '''
-    This iterates through our wordmap. For each key it pulls out the associated map for that key. 
-    We then iterate through our inner map and normalize our counts by dividing the current counts by the length of the inner map.
-    '''
-    def normalize_word_map(self):
-        #Outer map.
-        for key, values in self.wordMap.iteritems():
-            #Inner Map
-            for word_succeed, count in values.iteritems():
-                #Normalize counts & cast to floats
-                count = float(count) / float(sum(values.itervalues()))
-                values[word_succeed] = count   
+        if os.path.isfile(datafile):
+            with open(datafile, 'r') as infile:
+                self.wordMap = json.load(infile)
+        else:
+            previous = None
+            with open(source, 'r') as f:
+                for line in f:
+                    for word in line.split():
+                        current = word.rpartition('_')[0]
+                        if previous is not None:
+                            if previous in self.wordMap.keys() is not None:
+                                if current in self.wordMap[previous].keys() is not None:
+                                    self.wordMap[previous][current] += 1
+                                else:
+                                    self.wordMap[previous][current] = 1
+                            else:
+                                self.wordMap[previous] = {current : 1}
+                        previous = current
+
+            with open(datafile, 'w') as outfile:
+                json.dump(self.wordMap, outfile)
 
     def generate_given(self, keyword, length):
         sys.stdout.write(keyword + " ")
@@ -57,8 +55,6 @@ class BiGram:
         return k
 
 if __name__ == '__main__':
-    # Initialize GUI
-    source = "training_dataset_small.txt"
+    source = "training_dataset.txt"
     biGram = BiGram(source)
-    #biGram.normalize_word_map()
     biGram.generate_given("In", 50)
