90e4d91f3c018729775bad804fa6ed8982aeae76
Jacob Gillespie <jacobgillespie@gmail.com>
2016-05-23 21:35:18 -0600

    Read in the file and build the dictionary.

--- pos-tag.py
+++ pos-tag.py
@@ -1,8 +1,25 @@
 class BiGram:
 
-    def __init__(self):
-        return
+    wordMap = {}
+
+    def __init__(self, source):
+        previous = None
+        with open(source, 'r') as f:
+            for line in f:
+                for word in line.split():
+                    current = word.rpartition('_')[0]
+                    #print current
+                    if previous is not None:
+                        if previous in self.wordMap.keys() is not None:
+                            if current in self.wordMap[previous].keys() is not None:
+                                self.wordMap[previous][current] += 1
+                            else:
+                                self.wordMap[previous][current] = 1
+                        else:
+                            self.wordMap[previous] = {current : 1}
+                    previous = current
 
 if __name__ == '__main__':
     # Initialize GUI
-    biGram = BiGram()
+    source = "training_dataset.txt"
+    biGram = BiGram(source)
